2ef8781e338cc05258cc1e84fffded9d3b9271b6
sebastian correa morales <correa.m.sebastian@gmail.com>
2015-12-11 14:18:29 -0500

    receiving messages to tweet

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -7,34 +7,43 @@
 
 
 
-     /* {
-        "twitter": null,
-        "displayName": "elkykelyn",
-        "twitterToken": "2147486352-M2t7o2jvpz1N2BD5bKBuCYi1PcA9Zt3DvMKxhcp",
-        "twitterSecret": "TQ5qWuXallZKjNiwJixE18iAGwaF3yaiqQ8ufuQlMBGHL",
-        "createdAt": "2015-12-10T20:28:00.088Z",
-        "updatedAt": "2015-12-10T20:28:00.088Z",
-        "id": 1
-      }*/
-    
+/* {
+   "twitter": null,
+   "displayName": "elkykelyn",
+   "twitterToken": "2147486352-M2t7o2jvpz1N2BD5bKBuCYi1PcA9Zt3DvMKxhcp",
+   "twitterSecret": "TQ5qWuXallZKjNiwJixE18iAGwaF3yaiqQ8ufuQlMBGHL",
+   "createdAt": "2015-12-10T20:28:00.088Z",
+   "updatedAt": "2015-12-10T20:28:00.088Z",
+   "id": 1
+ }*/
+
 
 var Twit = require('twit');
 module.exports = {
 
     tweet: function(req, res) {
-        var T = new Twit({
-            consumer_key: config.TWITTER_KEY,
-            consumer_secret: config.TWITTER_SECRET,
-            access_token: '2147486352-M2t7o2jvpz1N2BD5bKBuCYi1PcA9Zt3DvMKxhcp',
-            access_token_secret: 'TQ5qWuXallZKjNiwJixE18iAGwaF3yaiqQ8ufuQlMBGHL'
-        })
+        User.findOne(req.userId, function(err, user) {
+            var message = req.body.message;
+            var T = new Twit({
+                consumer_key: config.TWITTER_KEY,
+                consumer_secret: config.TWITTER_SECRET,
+                access_token: user.twitterToken, // '2147486352-M2t7o2jvpz1N2BD5bKBuCYi1PcA9Zt3DvMKxhcp',
+                access_token_secret: user.twitterSecret // 'TQ5qWuXallZKjNiwJixE18iAGwaF3yaiqQ8ufuQlMBGHL'
+            });
 
 
-        T.post('statuses/update', {
-            status: 'hello world!!!!'
-        }, function(err, data, response) {
-            console.log(data, err);
-        })
+            T.post('statuses/update', {
+                status: req.body.message
+            }, function(err, data, response) {
+                console.log(data, err);
+                res.status(200).end();
+            });
+
+        });
+
+
+
+
     }
 
 };
